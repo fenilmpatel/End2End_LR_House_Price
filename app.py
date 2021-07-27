7dd02fa2f5094bcf670865636ceac8948349d8e3
Fenil Patel <40297793+fenilmpatel@users.noreply.github.com>
2021-07-27 19:23:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 29 14:53:34 2020
-
-@author: fenil
-"""
-
-from flask import Flask
+from flask import Flask,render_template,url_for,request
+import numpy as np
 import os
+import pickle
+model = pickle.load(open('house_model.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_flask():
-    return 'Hello flask user, How are you?'
+def home():
 
-port = os.environ.get('PORT',5000)
+    return render_template('index1.html',title='Home')
 
-if __name__ == '__main__':
-    
-    app.run(debug=1,host='0.0.0.0',port=port)
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template('prediction.html', prediction_text=output)
+
+port = int(os.environ.get('PORT',5000))
+if __name__ == "__main__":
+    app.run(debug=1,host='0.0.0.0',port=port) # or True
